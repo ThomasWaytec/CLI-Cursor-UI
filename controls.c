7f7623982c08f719b8a3e59f7e33bd3806fe6fc3
pufithenoob <pufithenoob@gmail.com>
2023-05-23 22:37:08 +0200

    Try getch. It won't work because it waits for input

--- controls.c
+++ controls.c
@@ -1,1 +1,16 @@
 /* Experiment with different ways to get input from user */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main(void) {
+
+    char c;
+    c = getch();
+    printf("!");
+    printf("%c\n");
+    return 0;
+}
