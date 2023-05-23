a8850d187b148744a7705e3ecb7603aab354be22
pufithenoob <pufithenoob@gmail.com>
2023-05-23 23:27:57 +0200

    Parse input from getch

--- controls.c
+++ controls.c
@@ -9,8 +9,34 @@
 int main(void) {
 
     char c;
-    c = getch();
-    printf("!");
-    printf("%c\n");
+    
+    while (1) {
+
+        c = getch();
+        // parse arrows
+        if (c == 0 || c == 224) { // if the first value is esc
+            switch(getch()) { // the real value
+                case 72:
+                    printf("Arrow Up\n");
+                    // code for arrow up
+                    break;
+                case 80:
+                    printf("Arrow Down\n");
+                    // code for arrow down
+                    break;
+                case 75:
+                    printf("Arrow Right\n");
+                    // code for arrow right
+                    break;
+                case 77:
+                    printf("Arrow Left\n");
+                    // code for arrow left
+                    break;
+            }
+        }
+
+        printf("%d, %c\n", c, c);
+    }
+
     return 0;
 }
