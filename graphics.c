7a9175630c668bcac913b0890c0da923d3e54602
pufithenoob <pufithenoob@gmail.com>
2023-05-24 19:26:03 +0200

    Add resources tab with one resource

--- graphics.c
+++ graphics.c
@@ -1,16 +1,35 @@
+/* 
+Resources:
+
+    GetConsoleScreenBufferInfo:
+        https://stackoverflow.com/questions/6812224/getting-terminal-size-in-c-for-windows
+
+*/
+
+#include <stdio.h>
 #include <windows.h>
 
-int main(void) {
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        int columns, rows;
+void print_terminal_dimensions(void) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
 
-        while (1) {
+    while (1) {
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
         rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
         printf("columns: %d\n", columns);
         printf("rows: %d\n", rows);
-        }
+        printf("\n");
+        Sleep(3 * 1000);
+    }
+
+}
+
+
+
+int main(void) {
+
+
     return 0;
 }
