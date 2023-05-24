1f8a5f47d32af0b0dc59cd8bf4ab5e3b86f47dd4
pufithenoob <116014797+pufithenoob@users.noreply.github.com>
2023-05-24 21:36:12 +0200

    Delete temp.c

--- temp.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int main(void) {
-    void* std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    static const COORD top_left = {0, 0};
-    SetConsoleCursorPosition(std_handle, top_left);
-    return 0;
-}
