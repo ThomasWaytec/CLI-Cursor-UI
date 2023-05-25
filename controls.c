8f72bdc3647784bd5f1f2f7f6da8bc8bb93fdcb5
pufithenoob <pufithenoob@gmail.com>
2023-05-25 21:28:12 +0200

    Move controls.c usage example from controls.c to graphics.c

--- controls.c
+++ controls.c
@@ -24,30 +24,3 @@ void process_user_input(const int MIN, const int MAX, int* value_to_modify, cons
 
     
 }
-
-
-// Test/Usage
-int main(void) {
-
-
-    // create button mapping
-    int button_mapping[BUTTON_MAPPING_LENGHT] = {0};
-    button_mapping['a'] = 1;
-    button_mapping['b'] = 2;
-    button_mapping['c'] = -1;
-    button_mapping[ENTER_KEY] = 10;
-    button_mapping[SPACE_KEY] = -10;
-
-    // create cursor
-    int* cursor = calloc(1, __SIZEOF_INT__);
-    (*cursor) = 0;
-
-
-    while (1) {
-        process_user_input(0, 9, cursor, button_mapping);
-        printf("cursor=%d\n", *cursor);
-        Sleep(1 * 1000);
-    }
-
-    return 0;
-}
