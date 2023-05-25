34183851abb2b9c95bad3e31a13dd4c047be7e69
pufithenoob <pufithenoob@gmail.com>
2023-05-25 21:20:57 +0200

    Move macros from controls.c to controls.h

--- controls.c
+++ controls.c
@@ -2,20 +2,7 @@
 #include <conio.h>
 #include <windows.h>
 
-#define ESCAPE_VALUE 224
-#define ESCAPE_VALUE_NUM_PAD 0 
-#define ESC_KEY 27
-#define SPACE_KEY 32
-#define ENTER_KEY 13
-#define ARROW_UP 72
-#define ARROW_DOWN 80
-#define ARROW_LEFT 75
-#define ARROW_RIGHT 77
-
-#define BUTTON_MAPPING_LENGHT 255
-
-
-
+#include "controls.h"
 
 
 void process_user_input(const int MIN, const int MAX, int* value_to_modify, const int* button_mapping) {
--- controls.h
+++ controls.h
@@ -1,6 +1,17 @@
 #ifndef controls.h
 #define controls.h
 
+#define ESCAPE_VALUE 224
+#define ESCAPE_VALUE_NUM_PAD 0 
+#define ESC_KEY 27
+#define SPACE_KEY 32
+#define ENTER_KEY 13
+#define ARROW_UP 72
+#define ARROW_DOWN 80
+#define ARROW_LEFT 75
+#define ARROW_RIGHT 77
+
+#define BUTTON_MAPPING_LENGHT 255
 
 
 #endif
