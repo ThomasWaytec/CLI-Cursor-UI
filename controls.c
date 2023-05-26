ece63137df172c832b77e2960d93ee8ddc0b89f9
pufithenoob <pufithenoob@gmail.com>
2023-05-26 20:42:51 +0200

    Change all variable names containing button to key

--- controls.c
+++ controls.c
@@ -5,7 +5,7 @@
 #include "controls.h"
 
 
-void process_user_input(const int MIN, const int MAX, int* value_to_modify, const int* button_mapping) {
+void process_user_input(const int MIN, const int MAX, int* value_to_modify, const int* key_mapping) {
     
     
     if (!kbhit()) {return;}
@@ -15,7 +15,7 @@ void process_user_input(const int MIN, const int MAX, int* value_to_modify, cons
     if (user_key == ESCAPE_VALUE || user_key == ESCAPE_VALUE_NUM_PAD) {user_key = getch();}
 
 
-    int modified_value = (*value_to_modify) + button_mapping[user_key];
+    int modified_value = (*value_to_modify) + key_mapping[user_key];
     if (modified_value < MIN) {modified_value = MIN;}
     if (modified_value > MAX) {modified_value = MAX;}
 
