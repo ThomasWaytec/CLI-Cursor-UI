ed7bbd7eaa8f00ce7cf71f91970dc19f409e92f8
pufithenoob <pufithenoob@gmail.com>
2023-05-28 16:36:31 +0200

    Create Cursor struct and update KeyProperties comments

--- controls.h
+++ controls.h
@@ -15,11 +15,26 @@
 
 #define KEY_MAPPING_LENGHT 255
 
+
+
 typedef struct {
 
-    /* the value to add to the x_coord in [...] */
+    int x_coord;
+    int x_coord_min;
+    int x_coord_max;
+
+    int y_coord;
+    int y_coord_min;
+    int y_coord_max;
+} Cursor;
+
+
+
+typedef struct {
+
+    /* the value to add to the x_coord in the Cursor */
     int add_to_x_coord;
-    /* the value to add to the x_coord in [...] */
+    /* the value to add to the x_coord in the Cursor */
     int add_to_y_coord;
     /* if process_user_input runs in a loop specify which key(s) terminate the loop when pressed */
     bool terminate_loop;
