2f1babcf4f0ddaf471d063f0579fb60603d2e683
pufithenoob <pufithenoob@gmail.com>
2023-05-29 21:41:37 +0200

    Create test usage for interface.c

--- interface.c
+++ interface.c
@@ -89,7 +89,7 @@ void draw_grid_with_cursor(const size_t GRID_HEIGHT, const size_t GRID_LENGTH, c
     draw_grid(GRID_HEIGHT, GRID_LENGTH, grid, BASE_PADDING);
 
     Sleep(3);
-    
+
     grid[cursor.x_coord][cursor.y_coord] = temp;
     SetConsoleCursorPosition(STD_HANDLE, TOP_LEFT_CURSOR_POSITION);
     draw_grid(GRID_HEIGHT, GRID_LENGTH, grid, BASE_PADDING);
@@ -128,6 +128,7 @@ Cursor emulate_grid(const size_t GRID_HEIGHT, const size_t GRID_LENGTH, char*** 
     };
     
     system("cls");
+    printf("cos");
     while (!process_user_input(&cursor, key_mapping)) {draw_grid_with_cursor(GRID_HEIGHT, GRID_LENGTH, grid, cursor);}
     system("cls");
 
@@ -146,22 +147,29 @@ int main(void) {
 
 
     // create grid
-    const size_t GRID_HEIGHT = 10;
+    const size_t GRID_HEIGHT = 3;
     const size_t GRID_LENGTH = 3;
 
     char*** grid = calloc(GRID_HEIGHT, __SIZEOF_POINTER__);
+    /*
     for (size_t row = 0; row < GRID_HEIGHT; row++)
     {
+
         grid[row] = calloc(GRID_LENGTH, __SIZEOF_POINTER__);
 
         for (size_t column = 0; column < GRID_LENGTH; column++)
         {   
-            grid[row][column] = "Option X";
+            //grid[row][column] = "1x1";
         }
     }
+    */
+    for (size_t row = 0; row < GRID_HEIGHT; row++) {grid[row] = calloc(GRID_LENGTH, __SIZEOF_POINTER__);}
+   
+    grid[0][0] = "1x1"; grid[0][1] = "2x2"; grid[0][2] = "3x3";
+    grid[1][0] = "4x4"; grid[1][1] = "5x5"; grid[1][2] = "6x6";
+    grid[2][0] = "7x7"; grid[2][1] = "8x8"; grid[2][2] = "9x9";
 
-    grid[0][0] = "helloka";
-    
+
     Cursor cursor = emulate_grid(GRID_HEIGHT, GRID_LENGTH, grid);
 
     printf("x=%d ", cursor.x_coord);
