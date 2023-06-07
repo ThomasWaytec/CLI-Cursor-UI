3a7380a7f22b0629de424dd1da752791ac7de21f
ThomasWaytec <thomas.waytec@gmail.com>
2023-06-07 19:24:00 +0200

    Add resize_temrinal_window function to resize the window to a hard-coded size

--- src/Tic-Tac-Toe.c
+++ src/Tic-Tac-Toe.c
@@ -7,14 +7,24 @@
 
 #define BLANK " "
 
+
 void hide_terminal_cursor(void) {
 
     /* hide Windows terminal cursor */
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(STD_HANDLE, &cursorInfo);
     cursorInfo.bVisible = 0;
+    cursorInfo.dwSize = 1;
     SetConsoleCursorInfo(STD_HANDLE, &cursorInfo);
 
+}
+
+void resize_terminal_window(void) {    
+    /* set window size */
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    bool bAbsolute = true;
+    SMALL_RECT small_rect = {1, 50, 45, 65};
+    SetConsoleWindowInfo(hStdOut, bAbsolute, &small_rect);
 }
 
 char* get_user_chosen_board_size(void) {
@@ -57,9 +67,7 @@ bool is_player_win(char* player_symbol, Cursor* cursor, size_t BOARD_HEIGHT, siz
     /* row */
     for (int i = 0; i < BOARD_HEIGHT; i++) {
 
-        printf("%s\n", game_board[cursor->x_coord][i]);
-        
-        if (strcmp(game_board[cursor->x_coord][i], player_symbol) != 0) {printf("break!\n"); break;}
+        if (strcmp(game_board[cursor->x_coord][i], player_symbol) != 0) {break;}
 
         if (i == BOARD_LENGTH - 1) {return true;}
     }
@@ -116,6 +124,10 @@ int main(void) {
     
     hide_terminal_cursor();
 
+    resize_terminal_window();
+
+
+
     /* get the chosen board size from the user */
     char* chosen_board_size = get_user_chosen_board_size();
     int board_size = chosen_board_size[0] - '0'; /* convert board size to integer*/
@@ -139,14 +151,12 @@ int main(void) {
 
 
 
-    /* main gameplay loop */
     Cursor cursor;
-
     char* player_symbols[2] = {"X", "O"};
     size_t turn_count = 0;
     char* current_player_symbol = player_symbols[turn_count % 2]; 
     
-
+    /* main gameplay loop */
     do
     {
         
@@ -165,7 +175,6 @@ int main(void) {
              !is_a_tie(turn_count, BOARD_HEIGHT, BOARD_LENGTH));
     
         
-    printf("The End!\n");
     return 0;
 }
 
