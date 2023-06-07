10bcfa71ecef816690c36a4ed56d086b4c1c70c8
ThomasWaytec <thomas.waytec@gmail.com>
2023-06-07 12:49:06 +0200

    Rename variables containing grid to board so it makes more sense

--- src/Tic-Tac-Toe.c
+++ src/Tic-Tac-Toe.c
@@ -5,12 +5,14 @@
 #include "controls.h"
 #include "interface.h"
 
-bool is_space_occupied(char*** game_grid, Cursor cursor) {
-    if (game_grid[cursor.x_coord][cursor.y_coord] != " ")
+#define BLANK " "
+
+bool is_space_occupied(char*** game_board, Cursor cursor) {
+    if (game_board[cursor.x_coord][cursor.y_coord] != BLANK)
     {
         printf("!!SPACE OCCUPIED!!");
         Sleep(1500);
-        return true;    
+        return true;
     }
     return false;
 }
@@ -25,7 +27,7 @@ void hide_terminal_cursor(void) {
 
 }
 
-char* get_user_chosen_grid_size(void) {
+char* get_user_chosen_board_size(void) {
     
     /* define the grid dimensions */
     const size_t GRID_HEIGHT = 7;
@@ -57,28 +59,29 @@ char* get_user_chosen_grid_size(void) {
 }
 
 
+
 int main(void) {
     
     hide_terminal_cursor();
 
-    /* get the chosen grid size from the user */
-    char* chosen_grid_size = get_user_chosen_grid_size();
-    int grid_size = chosen_grid_size[0] - '0'; /* get grid size as integer*/
+    /* get the chosen board size from the user */
+    char* chosen_board_size = get_user_chosen_board_size();
+    int board_size = chosen_board_size[0] - '0'; /* convert board size to integer*/
 
 
 
-    /* create game grid of chosen size */
-    const size_t GRID_HEIGHT = grid_size;
-    const size_t GRID_LENGTH = grid_size;
+    /* create game board of chosen size */
+    const size_t BOARD_HEIGHT = board_size;
+    const size_t BOARD_LENGTH = board_size;
 
-    char*** game_grid = calloc(GRID_HEIGHT, __SIZEOF_POINTER__); /* allocating space for the grid */
+    char*** game_board = calloc(BOARD_HEIGHT, __SIZEOF_POINTER__); /* allocating space for the board */
 
-    for (size_t row = 0; row < GRID_HEIGHT; row++) {
-        game_grid[row] = calloc(GRID_LENGTH, __SIZEOF_POINTER__);
+    for (size_t row = 0; row < BOARD_HEIGHT; row++) {
+        game_board[row] = calloc(BOARD_LENGTH, __SIZEOF_POINTER__);
 
-        for (size_t column = 0; column < GRID_LENGTH; column++)
+        for (size_t column = 0; column < BOARD_LENGTH; column++)
         {
-            game_grid[row][column] = " ";     /* initializing the grid */
+            game_board[row][column] = " ";     /* initializing the board */
         }
     }
 
@@ -94,15 +97,15 @@ int main(void) {
     while (!game_end)
     {
         
-        if (turn_counter == GRID_HEIGHT * GRID_LENGTH) {printf("Tie!"); break;}
+        if (turn_counter == BOARD_HEIGHT * BOARD_LENGTH) {printf("Tie!"); break;}
         
         current_player_symbol = player_symbols[turn_counter % 2];
-        cursor = live_grid(GRID_HEIGHT, GRID_LENGTH, game_grid);
+        cursor = live_grid(BOARD_HEIGHT, BOARD_LENGTH, game_board);
 
         /* check if space is occupied */
-        if (is_space_occupied(game_grid, cursor)) {continue;}
+        if (is_space_occupied(game_board, cursor)) {continue;}
         
-        game_grid[cursor.x_coord][cursor.y_coord] = current_player_symbol;
+        game_board[cursor.x_coord][cursor.y_coord] = current_player_symbol;
         turn_counter++;
 
     }
